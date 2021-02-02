80a3174f77c9169393064cfdee9d92efd5850ef0
Nizamet Özkan <nizamet005@gmail.com>
2021-02-03 01:08:39 +0300

    Initial Commit

---
+++ SimyaStagram/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimyaStagram
+//
+//  Created by Nizamet Özkan on 3.02.2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
