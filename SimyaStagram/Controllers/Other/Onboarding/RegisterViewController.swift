fa09bbeb6d1871994d9f626d57e3f8d9b6ee1994
Nizamet Özkan <nizamet005@gmail.com>
2021-02-06 09:40:04 +0300

    Login & Register complated.

--- SimyaStagram/Controllers/Other/Onboarding/RegisterViewController.swift
+++ SimyaStagram/Controllers/Other/Onboarding/RegisterViewController.swift
@@ -9,71 +9,122 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
-    private let usernameEmailField: UITextField = {
-       
-        return UITextField()
+    struct Constants{
+        static let cornerRadius: CGFloat = 8.0
+    }
+    
+    private let usernameField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Username..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        return field
+    }()
+    
+    private let EmailField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Email..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        return field
     }()
     
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
-        return UITextField()
+        field.placeholder = "Password..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        return field
     }()
     
-    private let loginButton: UIButton = {
+    private let registerButton: UIButton = {
         let button = UIButton()
-        
+        button.setTitle("Log in", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.black, for: .normal)
         return button
-    }()
-    
-    private let termsButton: UIButton = {
-        let button = UIButton()
-        
-        return button
-    }()
-    
-    private let privacyButton: UIButton = {
-        let button = UIButton()
-        
-        return button
-    }()
-    
-    private let createAccountButton: UIButton = {
-        let button = UIButton()
-        
-        return button
-    }()
-    
-    private let headerView: UIView = {
-        let view = UIView()
-        
-        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addSubViews()
+
+        registerButton.addTarget(self, action: #selector(didTapRegister), for: .touchUpInside)
+        usernameField.delegate = self
+        EmailField.delegate = self
+        passwordField.delegate = self
+        view.addSubview(usernameField)
+        view.addSubview(EmailField)
+        view.addSubview(passwordField)
+        view.addSubview(registerButton)
         view.backgroundColor = .systemBackground
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        usernameField.frame = CGRect(x: 20, y: view.safeAreaInsets.top+10, width: view.width-40, height: 52.0)
+        EmailField.frame = CGRect(x: 20, y: usernameField.bottom+10, width: view.width-40, height: 52.0)
+        passwordField.frame = CGRect(x: 20, y: EmailField.bottom+10, width: view.width-40, height: 52.0)
+        registerButton.frame = CGRect(x: 20, y: passwordField.bottom+20, width: view.width-40, height: 52.0)
+    }
+    
+    @objc func didTapRegister() {
+        usernameField.resignFirstResponder()
+        EmailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
         
+        guard let username = usernameField.text, !username.isEmpty,
+              let email = EmailField.text, !email.isEmpty,
+              let password = passwordField.text, !email.isEmpty, password.count >= 8 else {
+            return
+        }
+        
+        AuthManager.shared.registerUser(username: username, email: email, password: password) { registered in
+            if registered {
+                
+            }else{
+                
+            }
+            
+        }
     }
-    
-    func addSubViews(){
-        view.addSubview(usernameEmailField)
-        view.addSubview(passwordField)
-        view.addSubview(loginButton)
-        view.addSubview(termsButton)
-        view.addSubview(privacyButton)
-        view.addSubview(createAccountButton)
-        view.addSubview(headerView)
+}
+
+extension RegisterViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameField {
+            EmailField.becomeFirstResponder()
+        }
+        else if textField == EmailField {
+            passwordField .becomeFirstResponder()
+        }
+        
+        return true
     }
-    
-    @objc private func didTapLoginButton() {}
-    @objc private func didTapTermsButton() {}
-    @objc private func didTapPrivacyButton() {}
-    @objc private func didTapCreateAccountButton() {}
-    
 }
--- SimyaStagram/Resources/DatabaseManager.swift
+++ SimyaStagram/Resources/DatabaseManager.swift
@@ -6,10 +6,27 @@
 //
 
 import FirebaseDatabase
+import FirebaseAuth
 
 public class DatabaseManager {
     
     static let shared = DatabaseManager()
     
+    private let database = Database.database().reference()
     
+    public func registerNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        completion(true)
+    }
+    
+    public func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        database.child(email.safeDatabaseKey()).setValue(["username": username]) { error, _ in
+            if error == nil {
+                completion(true)
+                return
+            } else {
+                completion(false)
+                return
+            }
+        }
+    }
 }
